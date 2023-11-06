a51f85e094538f5f3828fc35dfada1131d6925ab
Tsyporra <guillard.maty@gmail.com>
2023-11-06 13:43:03 +0100

    affichage de tous les films en back

--- src/Controller/Back/MovieController.php
+++ src/Controller/Back/MovieController.php
@@ -2,19 +2,27 @@
 
 namespace App\Controller\Back;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\MovieRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+    /**
+     * @Route("/back/movie")
+     */
 class MovieController extends AbstractController
 {
     /**
-     * @Route("/back/movie", name="app_back_movie")
+     * READ
+     * Affiche tous les films
+     * @Route("/", name="app_back_movie")
      */
-    public function index(): Response
+    public function index(MovieRepository $movieRepository): Response
     {
+        $movies = $movieRepository->findAll();
+        
         return $this->render('back/movie/index.html.twig', [
-            'controller_name' => 'MovieController',
+            'movies' => $movies,
         ]);
     }
 }
